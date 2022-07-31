a8e6652a5c2aff2dceb5b2e5dc5d223df5f78cab
Paulina-Maciesowicz <paulinamaa@gmail.com>
2022-07-31 18:01:00 +0200

    update
    all task goit-markup-hw-05

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,4 @@
-<html>
-  <head> </head>
-  <body>
-    <button data-modal-open>Zamów usługę</button>
-    <button data-modal-close type="button">X</button>
-    <div class="backdrop is-hidden" data-modal>modal</div>
-  </body>
- 
-    <!-- 
-    w inspektorze wklejamy w zakładce console/konsola 
-    document.querySelector("[data-modal-open]")
-    i jeśli pojawi się button to jest ok
 
-    document.querySelector("[data-modal-open]").getAttribute('data-modal-open')
-    --></html>
-
- /*
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
@@ -29,4 +13,3 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
-*/
